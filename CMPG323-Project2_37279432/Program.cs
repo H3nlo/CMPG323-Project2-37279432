171747b52174a5cd9441d6427e26c9fa84bdd899
H3nlo <111774279+H3nlo@users.noreply.github.com>
2023-08-29 23:43:25 +0200

    Updates to controllers

--- CMPG323-Project2_37279432/Program.cs
+++ CMPG323-Project2_37279432/Program.cs
@@ -5,14 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Project2AZUREDBContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//builder.Services.AddDbContext<ProductsDBContext>(options => options.UseSqlServer(connectionString));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
