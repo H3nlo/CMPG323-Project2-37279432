3e19a0037878e118bdd8848a85169f8013575caf
H3nlo <111774279+H3nlo@users.noreply.github.com>
2023-08-30 17:44:38 +0200

    Added controllers

--- CMPG323-Project2-37279432/Program.cs
+++ CMPG323-Project2-37279432/Program.cs
@@ -1,8 +1,14 @@
+using CMPG323_Project2_37279432.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Project2AZUREDBContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
